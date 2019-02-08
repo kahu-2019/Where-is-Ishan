6c540ed0427e24b6a498d60ba3ab5f09f08d1938
student <you@eda.com>
2019-02-08 13:43:34 +1300

    seed updated with zoom

--- client/api/apiRoute.js
+++ client/api/apiRoute.js
@@ -1,18 +1,18 @@
 import request from 'superagent'
 
 
-apiImgReq = `https://maps.googleapis.com/maps/api/staticmap?center=${latLong}&${zoom}&scale=1&size=600x300&maptype=satellite&format=png&visual_refresh=true&key=AIzaSyAHciFTN54JS1w-iheiqpAN47wvNbx889I`
+apiImgReq = `https://maps.googleapis.com/maps/api/staticmap?center=${latLong}&zoom=${zoom}&scale=1&size=600x300&maptype=satellite&format=png&visual_refresh=true&key=AIzaSyAHciFTN54JS1w-iheiqpAN47wvNbx889I`
 
 randy = () => {
-	return rand = Math.floor(Math.random() * 10) + 1  
+	return rand = Math.floor(Math.random() * 10) + 1
 }
 
-export function getPlaceName(randy){
+export function getPlaceName(randy) {
 	return request.get('/api')
-	.then(
-		res => {
-			console.log(res.body)
-			return res.body
-		}
-	)
+		.then(
+			res => {
+				console.log(res.body)
+				return res.body
+			}
+		)
 }
