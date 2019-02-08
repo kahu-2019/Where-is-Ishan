5d4e7e4f25325f27bdede95f7db12c948f020dd9
Rebecca Jackson <zimbecs@gmail.com>
2019-02-08 15:17:04 +1300

    button names working

--- client/api/apiRoute.js
+++ client/api/apiRoute.js
@@ -2,27 +2,25 @@ import request from 'superagent'
 
 // apiImgReq = `https://maps.googleapis.com/maps/api/staticmap?center=${latLong}&${zoom}&scale=1&size=600x300&maptype=satellite&format=png&visual_refresh=true&key=AIzaSyAHciFTN54JS1w-iheiqpAN47wvNbx889I`
 
-// randy = () => {
-// 	return rand = Math.floor(Math.random() * 10) + 1  
-// }//really helpful if it can get three numbers
 
-function generateRandomArr(truePlaceId){
+export function getPlaceName(){
 	var randy = []
+	let trueId = 3
+
+	
 	while(randy.length < 3){
-    var r = Math.floor(Math.random()*10) + 1;
-    if(!truePlaceNum && randy.indexOf(r) === -1 ) randy.push(r);
+    var r = Math.ceil(Math.random()*10);
+    if(r != trueId && randy.indexOf(r) === -1 ) randy.push(r);
 	}
-	randy.push(truePlaceId)
+	randy.push(trueId)
+
+	
+
 	console.log(randy)
-	return randy
-}
-
-
-export function getPlaceName(generateRandomArr){
-	return request.get('/api')
+	// return randy
+	return request.post('/api', randy)
 		.then(
 			res => {
-				console.log(res.body)
 				return res.body
 			}
 		)
